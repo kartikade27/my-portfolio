0735a5044f1dd41d03dc7635ac4bcefb72256964
kartikade27 <kartikade27@gmail.com>
2026-03-10 12:12:51 +0530

    modified update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -24,7 +24,7 @@ const Project = () => {
         "Cloudinary",
       ],
       imageUrl: eventImg,
-      githubLink: "https://github.com/kartikade27/personal-finance-backend",
+      githubLink: "https://github.com/kartikade27/employee-management-frontend",
       previewLink:
         "https://kartikade27.github.io/employee-management-frontend/",
     },
